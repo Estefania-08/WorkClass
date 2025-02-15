a683dc66323dae49fdbf417cce0cb8d37bcd5a4f
Estefania-08 <estefaniajimenezg3@gmail.com>
2025-02-14 22:38:54 -0600

    added new classes and new buttons

--- WorkClass/app/src/main/java/com/example/workclass/MainActivity.kt
+++ WorkClass/app/src/main/java/com/example/workclass/MainActivity.kt
@@ -3,27 +3,27 @@ package com.example.workclass
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -31,6 +31,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import com.example.workclass.ui.screens.HomeScreen
+import com.example.workclass.ui.screens.MainMenuScreen
+import com.example.workclass.ui.screens.TestScreen
 import com.example.workclass.ui.theme.WorkClassTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +46,8 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             WorkClassTheme {
-                Column() {
+                ComposeMultiScreenApp()
+               /*Column() {
                     Column() {
                         TextComposable("Roberto")
                         TextComposable()
@@ -57,9 +65,10 @@ class MainActivity : ComponentActivity() {
                         ModifierExample2()
                         ModifierExample4()
                         CustomText()
+                        picture()
 
                     }
-                }
+                }*/
             }
         }
     }
@@ -162,6 +171,43 @@ fun CustomText() {
         )
     }
 }
+@Preview(showBackground = true)
+@Composable
+fun picture(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+            .height(300.dp)
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.si),
+            contentDescription = "Logo Android",
+            contentScale = ContentScale.FillHeight
+        )
+    }
+}
+
+@Composable
+fun ComposeMultiScreenApp(){
+    val navController = rememberNavController()
+    SetupNavGraph(navController= navController)
+}
+
+@Composable
+fun SetupNavGraph(navController: NavHostController){
+    NavHost(navController = navController, startDestination = "main_menu") {
+    composable("main_menu"){ MainMenuScreen(navController)}
+        composable("home_screen"){ HomeScreen(navController)}
+        composable("test_screen"){ TestScreen(navController)}
+
+    }
+}
+
+
+
 
 
 
